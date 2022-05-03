ba6729df7ecff0f86f04d72d0d5d5e72ed4401e0
minhngo <tngo238@student.cccd.edu>
2022-05-03 14:30:55 -0700

    update test parsing urls

--- WebParsing.py
+++ WebParsing.py
@@ -2,11 +2,23 @@ from bs4 import BeautifulSoup
 from requests_html import HTML, HTMLSession 
 
 
-dummy_url = 'https://www.amazon.com/s?k=laptop&crid=199AKFS927SCF&sprefix=laptop%2Caps%2C131&ref=nb_sb_noss_1'
+dummy_url = 'https://www.etsy.com/c/home-and-living?ref=catnav-891'
+
+test_url = 'https://www.etsy.com/c/home-and-living?ref=catnav-891'
 
 session = HTMLSession()
-r = session.get(dummy_url)
 
-div = r.html.find('div data-asin', first=True)
+r = session.get(test_url)
 
-print(div.html)
+list_items = r.html.find('p')   # return a list
+# access syntax tag .class-name
+
+abs_links = r.html.absolute_links        # return a set
+# a set = r.html.links                    get links without full url in case of sub items
+
+div_content = r.html.find('div#content')     # return initial div tag with id='content' 
+
+links_content = r.html.find('#content', first=True)     # Add first=True to get the first element -> allow to access as html
+#print(links_content.tag)   return the found tag  
+
+extracted_cards = links_content.find('a')       # return a list
