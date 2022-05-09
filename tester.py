dede2fa5f5924c524af324157b468ae568d87188
minhngo <minhngo2814@gmail.com>
2022-05-08 22:47:37 -0700

    new function and prototype

--- tester.py
+++ tester.py
@@ -1,9 +1,3 @@
-def testTotalTraversing(result, raw_parsed_output):
-  if len(result) == len(raw_parsed_output):
-    print('Traversed all items checked!', '\n')
-  else:
-    print('Funtion did not go through!', '\n')
-
 # Print items in the list
 def printContents(parsed_list):
   for item in parsed_list:
